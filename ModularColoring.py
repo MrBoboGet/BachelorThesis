564ca9b00b3b31b834c78552b4bc0c60b7c7d783
MrBoboGet <emanuelberggren01@gmail.com>
2023-03-27 21:56:41 +0200

    Injective map on child colors, not child nodes

--- ModularColoring.py
+++ ModularColoring.py
@@ -1,22 +1,23 @@
 import ModularDecomposition.modularDecomp as md
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# deterministic resultts
+import random
+import numpy
+
+random.seed(246)        # or any integer
+numpy.random.seed(4812)
 
 def first(Iterable):
     for x in Iterable:
         return(x)
-# hueristic, takes a induced subgraph along with already colored parts
-# and then colors the rest.
 
-# Returns a list of colors, and the chromatic number
-def Colorize(OriginalGraph,MD,Heuristic):
-    Root = max([module for module in MD],key=lambda x: len(x))
-    Colors = [i for i in range(len(Root))]
-    ColorMap = Colorize_Module(MD,Colors,Root)
-    return (Colors,len(ColorMap))
 
 # returns the color map
 def Colorize_Module(OriginalGraph,MD,ColorList,ModuleToColor,Heuristic):
     if(len(ModuleToColor) == 1):
-        return(set(ColorList[ [x for x in ModuleToColor][0]]}))
+        return(set( [ ColorList[first(ModuleToColor)] ]))
     ReturnValue = set()
     Label = MD.nodes[ModuleToColor]['MDlabel']
     ChildrenColorMap = {}
@@ -31,7 +32,7 @@ def Colorize_Module(OriginalGraph,MD,ColorList,ModuleToColor,Heuristic):
                 ReturnValue.add(Color)
     elif (Label == '0'):
         #series
-        MaxMap = max(ChildrenColorMap,key=lambda x: len(x))
+        MaxMap = max(ChildrenColorMap.values(),key=lambda x: len(x))
 
         for (Nodelist,Colors) in ChildrenColorMap.items():
             #recolor, random injective map
@@ -39,19 +40,63 @@ def Colorize_Module(OriginalGraph,MD,ColorList,ModuleToColor,Heuristic):
             for (Original,New) in zip(Colors,MaxMap):
                 InjectiveMap[Original] = New
             for Node in Nodelist:
-                ColorMap[Node] = InjectiveMap[ColorMap[Node]]
+                ColorList[Node] = InjectiveMap[ColorList[Node]]
             
         ReturnValue = MaxMap
 
     else:
         #prime
-
         #remove coloring for trivial modules, these are the ones we can recolor
-        for Child in MB.nodes[[ModuleToColor]:
-            if(len(Child) == 1):
-                ColorList[first(Child)] = -1
-        ReturnValue = Heuristic(ColorList,OriginalGraph,Child)
+        for Child in ChildrenColorMap:
+            for Color in ChildrenColorMap[Child]:
+                ReturnValue.add(Color)
+
+        for Child in ModuleToColor:
+            ColorList[Child] = -1
+        ReturnValue = Heuristic(nx.induced_subgraph(OriginalGraph,ModuleToColor),ColorList,ReturnValue)
     return(ReturnValue)
 
-def Greedy():
-    pass
+# Returns a list of colors, and the chromatic number
+def Colorize(OriginalGraph,MD,Heuristic):
+    Root = max([module for module in MD],key=lambda x: len(x))
+    Colors = [i for i in range(len(Root))]
+    ColorMap = Colorize_Module(OriginalGraph,MD,Colors,Root,Heuristic)
+    return (Colors,ColorMap)
+
+# hueristic, takes a induced subgraph along with already colored parts
+# and then colors the rest.
+def Greedy(GraphToColor: nx.DiGraph,CurrentColoring,ColorList):
+    ReturnValue = set()
+    #completely arbitrary order
+    for Node in GraphToColor.nodes:
+        for Color in ColorList:
+            ColorIsValid = True
+            for Neighbour in GraphToColor.adj[Node]:
+                if(CurrentColoring[Neighbour] == Color):
+                    ColorIsValid = False
+                    break
+            if(ColorIsValid):
+                CurrentColoring[Node] = Color
+                ReturnValue.add(Color)
+                break
+    return(ReturnValue)
+
+
+def VerifyColoring(Graph,Coloring):
+    ReturnValue = True
+    for Node in Graph.nodes:
+        for Neighour in Graph.adj[Node]:
+            if(Coloring[Node] == Coloring[Neighour]):
+                return(False)
+    return(ReturnValue)
+
+edge_prob = 0.1
+G = nx.fast_gnp_random_graph(10, edge_prob)
+MD = md.modularDecomposition(G)
+(Colors,ColorCount) = Colorize(G,MD,Greedy)
+print(ColorCount,len(ColorCount),Colors)
+print(VerifyColoring(G,Colors))
+
+nx.write_adjlist(G,"ADJList.txt")
+nx.draw(G)
+plt.show()
