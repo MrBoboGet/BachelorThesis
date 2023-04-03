4453c735eb2ccb31a63d5b88211d7020dc8fb527
MrBoboGet <emanuelberggren01@gmail.com>
2023-04-03 22:49:11 +0200

    Runtests now utilizes multiple jobs

--- RunTests.sh
+++ RunTests.sh
@@ -1,5 +1,22 @@
 
-for EdgeFile in ./Data/*.edge; do
-    echo "${EdgeFile}: "
-    py ModularColoring.py $EdgeFile
+
+if [[ -d TempOutput ]]; then
+    rm -r TempOutput
+fi
+mkdir TempOutput
+declare -i i=0
+for EdgeFile in Data/*.edge; do
+    ResFile=${EdgeFile#Data/}
+    py ModularColoring.py $EdgeFile > "TempOutput/${ResFile/%.edge/.res}" &
+    Jobs[$i]=$!
+    i+=1
+done
+
+for pid in ${Jobs[*]}; do
+    echo "Job finished $pid"
+    wait $pid
+done
+
+for Res in ./TempOutput/*.res; do
+    cat "$Res" >> TotalOutput.csv
 done
